6bd0a0a85876f71bdf874e70dc441cb833b1bfd2
VKalenski <vilislavkalenski@gmail.com>
2025-05-01 00:34:48 +0300

    Edit base k8s files and edit the minikube workflow

---
+++ k8s/base-resources/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAMESPACE=test
+
+kubectl apply -f namespace.yaml
+kubectl apply -f deployment.yaml -n $NAMESPACE
+kubectl apply -f service.yaml -n $NAMESPACE
+kubectl apply -f ingress.yaml -n $NAMESPACE
+
+echo "✅ Deployment completed successfully in namespace '$NAMESPACE'"
