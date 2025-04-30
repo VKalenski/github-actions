9f0778af2a20a2cbe94338f51bc78fe0352930ba
VKalenski <vilislavkalenski@gmail.com>
2025-05-01 00:53:27 +0300

    Edit the minikube workflow and k8s base files

--- k8s/base-resources/deploy.ps1
+++ k8s/base-resources/deploy.ps1
@@ -1,9 +1,34 @@
-$namespace = "test"
+$namespace_test = "test"
+$namespace_dev = "dev"
+$namespace_val = "val"
 
 kubectl apply -f namespace.yaml
-kubectl apply -f deployment.yaml -n $namespace
-kubectl apply -f service.yaml -n $namespace
-kubectl apply -f ingress.yaml -n $namespace
-kubectl apply -f storageclass.yaml -n $namespace
 
-Write-Host "✅ Deployment completed successfully in namespace '$namespace'"
+# 🎯 Applying resources to 'test' namespace
+Write-Host "🎯 Applying resources to 'test' namespace"
+kubectl apply -f deployment.yaml -n $namespace_test
+kubectl apply -f service.yaml -n $namespace_test
+kubectl apply -f ingress.yaml -n $namespace_test
+kubectl apply -f storageclass.yaml -n $namespace_test
+
+Write-Host "✅ Deployment completed successfully in namespace '$namespace_test'"
+Write-Host "----------------------------------------------------------------"
+
+# 🎯 Applying resources to 'dev' namespace
+Write-Host "🎯 Applying resources to 'dev' namespace"
+kubectl apply -f deployment.yaml -n $namespace_dev
+kubectl apply -f service.yaml -n $namespace_dev
+kubectl apply -f ingress.yaml -n $namespace_dev
+kubectl apply -f storageclass.yaml -n $namespace_dev
+
+Write-Host "✅ Deployment completed successfully in namespace '$namespace_dev'"
+Write-Host "----------------------------------------------------------------"
+
+# 🎯 Applying resources to 'val' namespace
+Write-Host "🎯 Applying resources to 'val' namespace"
+kubectl apply -f deployment.yaml -n $namespace_val
+kubectl apply -f service.yaml -n $namespace_val
+kubectl apply -f ingress.yaml -n $namespace_val
+kubectl apply -f storageclass.yaml -n $namespace_val
+
+Write-Host "✅ Deployment completed successfully in namespace '$namespace_val'"
