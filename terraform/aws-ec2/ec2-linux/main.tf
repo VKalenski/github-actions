20962a2cfe74f8b332cff1eec7758d67229d0474
VKalenski <vilislavkalenski@gmail.com>
2025-05-04 12:32:11 +0300

    Move terraform scripts into diff folders

---
+++ terraform/aws-ec2/ec2-linux/main.tf
@@ -0,0 +1,36 @@
+resource "aws_key_pair" "my_key" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name_prefix = var.security_group_name
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "my-free-linux-vm"
+  }
+}
---
+++ terraform/aws-ec2/ec2-linux/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-004f2229fb9afa698"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key to register in AWS"
+  type        = string
+  default     = "aws_terraform_key"
+}
+
+variable "public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/aws_terraform_key.pub"
+}
+
+variable "security_group_name" {
+  description = "Name prefix for the security group"
+  type        = string
+  default     = "allow-ssh"
+}
